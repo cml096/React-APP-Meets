cb9175ab909b0b8d2442853f783094fbe35c27f8
Cristobal <cristobalmontanolopez@gmail.com>
2024-04-29 09:01:03 -0400

    add full project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <FavoritesContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavoritesContextProvider>
+);
